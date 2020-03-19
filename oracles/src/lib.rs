148d997c2b7cbe52ebc2d50bd5f758a4f9e02825
Evgeny Ukhanov <mrlsd@ya.ru>
2020-03-19 21:36:16 +0200

    Extend Oracles CoinMarget data struct

--- oracles/src/lib.rs
+++ oracles/src/lib.rs
@@ -8,16 +8,16 @@ use serde::Deserialize;
 use url::Url;
 use xpx_supercontracts_sdk::{
 	http::{http_get, HttpRequest},
+	storage::storage_get,
 	transactions_type::FUNCTION_ERROR,
 	utils::debug_message,
-	storage::storage_get
 };
 
 const API_KEY_FILE: &str = "coinmarket.key";
 const URL_LIST: &str = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
 
 #[derive(Debug, Deserialize)]
-struct MarketPriceStruct {
+struct MarketPriceStatus {
 	timestamp: String,
 	error_code: i64,
 	error_message: Option<String>,
@@ -26,8 +26,42 @@ struct MarketPriceStruct {
 }
 
 #[derive(Debug, Deserialize)]
+struct MarketUSDData {
+	price: f64,
+	volume_24h: f64,
+	percent_change_1h: f64,
+	percent_change_24h: f64,
+	percent_change_7d: f64,
+	market_cap: f64,
+	last_updated: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct MarketUSDQuote {
+	#[serde(rename = "USD")]
+	usd: MarketUSDData,
+}
+
+#[derive(Debug, Deserialize)]
+struct MarketPriceData {
+	id: i64,
+	name: String,
+	symbol: String,
+	slug: String,
+	num_market_pairs: i64,
+	date_added: String,
+	tags: Vec<String>,
+	circulating_supply: f64,
+	total_supply: f64,
+	cmc_rank: i64,
+	last_updated: String,
+	quote: MarketUSDQuote,
+}
+
+#[derive(Debug, Deserialize)]
 struct MarketPrice {
-	status: MarketPriceStruct,
+	status: MarketPriceStatus,
+	data: Option<Vec<MarketPriceData>>,
 }
 
 #[no_mangle]
@@ -50,13 +84,15 @@ pub extern "C" fn get_market_info() -> i64 {
 		debug_message(&format!("failed parse URL: {:?}", err));
 		return FUNCTION_ERROR;
 	}
+
 	let mut base_url = res.unwrap();
 	let start = 1;
-	let limit = 1;
+	let limit = 3;
 	let currency = "USD";
-	base_url.set_query(Some(&format!("start={}", start)));
-	base_url.set_query(Some(&format!("limit={}", limit)));
-	base_url.set_query(Some(&format!("currency={}", currency)));
+	base_url.query_pairs_mut()
+		.append_pair("convert", currency)
+		.append_pair("limit", format!("{}", limit).as_str())
+		.append_pair("start", format!("{}", start).as_str());
 
 	debug_message(&format!("Request URL: {:?}", base_url));
 
@@ -75,7 +111,6 @@ pub extern "C" fn get_market_info() -> i64 {
 	}
 
 	let content = resp.unwrap();
-	debug_message(&format!("Request content: {:?}", content));
 
 	let res = serde_json::from_slice(&content[..]);
 	if let Err(err) = res {
@@ -83,6 +118,11 @@ pub extern "C" fn get_market_info() -> i64 {
 		return FUNCTION_ERROR;
 	}
 	let info: MarketPrice = res.unwrap();
+	if info.status.error_code != 0 {
+		debug_message(&format!("Failed get market info: {}", info.status.error_message.unwrap()));
+		return FUNCTION_ERROR;
+	}
+
 	debug_message(&format!("Market info: {:#?}", info));
 
 	content.len() as i64
